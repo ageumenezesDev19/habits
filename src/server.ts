a5dc2c0771f59a028e29159d5ea384540a5e920e
ageu_dev19 <ageumenezes423@gmail.com>
2023-12-30 08:25:09 -0300

    Finish the first part of the app back-end

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,26 @@
-import fastify from "fastify";
+import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+import fastify from "fastify"
 
 const app = fastify();
+const prisma = new PrismaClient();
 
-app.get('/', () => {
-  return 'Hello World';
+app.register(cors);
+
+app.get('/hello', async () => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: 'Beber'
+      }
+    }
+  })
+
+  return habits
 });
 
 app.listen({
   port: 3333,
+}).then(() => {
+  console.log('HTTP server running!')
 });
